e895fae020ab3de226bc84af17f4c6210902d8a3
Szymon Miękus <miekusoft@gmail.com>
2023-01-03 21:49:38 +0100

    Use lib in test proj

--- utils/src/array/doubles.ts
+++ utils/src/array/doubles.ts
@@ -1,4 +1,12 @@
-const __doubles = (arr: number[]) => {
+/**
+ * Creates an array of elements multiplied by x2
+ * @param {Array} arr of numbers
+ * @returns {Array} Returns the new array of diltred values
+ * @example
+ * __doubles([1, 2, 3, 4, 5]);
+ * // => [2, 4, 6, 8, 10]
+ */
+const __doubles = (arr: number[]): number[] => {
   return arr.map((x) => x * 2);
 };
 
